38feafa2a99babbc00c739a0617633528fb31cb6
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2017-07-24 11:33:17 +0200

    Make it usable on puppet3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class dell (
-  String            $omsa_url_base           = $dell::params::omsa_url_base,
-  String            $omsa_url_args_indep     = $dell::params::omsa_url_args_indep,
-  String            $omsa_url_args_specific  = $dell::params::omsa_url_args_specific,
-  String            $omsa_version            = $dell::params::omsa_version,
-  String            $customplugins           = $dell::params::customplugins,
-  String            $check_warranty_revision = $dell::params::check_warranty_revision,
-  Boolean           $manage_debian_apt       = $dell::params::manage_debian_apt,
-  Optional[String]  $api_key                 = undef,
+  $omsa_url_base           = $dell::params::omsa_url_base,
+  $omsa_url_args_indep     = $dell::params::omsa_url_args_indep,
+  $omsa_url_args_specific  = $dell::params::omsa_url_args_specific,
+  $omsa_version            = $dell::params::omsa_version,
+  $customplugins           = $dell::params::customplugins,
+  $check_warranty_revision = $dell::params::check_warranty_revision,
+  $manage_debian_apt       = $dell::params::manage_debian_apt,
+  $api_key                 = undef,
 ) inherits ::dell::params {
   validate_absolute_path($customplugins)
 
